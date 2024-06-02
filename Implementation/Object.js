a752177ec0fe89cce82f3e157fe6392ea697bfc3
Mayur <mayursonare18@gmail.com>
2024-06-02 16:06:24 +0530

    object

--- Implementation/Object.js
+++ Implementation/Object.js
@@ -1,11 +1,11 @@
 const obj = {
-    name: 'John',
+    name: 'John Doe',
     age: 30,
     city: 'New York'
 }
-console.log(Object.keys(obj)) // Returns an array of a given object's own enumerable property names
-console.log(Object.values(obj)) // Returns an array of a given object's own enumerable property values
-console.log(Object.entries(obj)) // Returns an array of a given object's own enumerable string-keyed property [key, value] pairs
-console.log(Object.assign(obj, {name: 'Jane'})) // Copies all enumerable own properties from one or more source objects to a target object
-console.log(obj.name)
-console.log(obj.age)
+console.log(Object.keys(obj)) 
+console.log(Object.values(obj))
+console.log(Object.entries(obj)) 
+console.log(Object.assign(obj, {name: 'Jane'})) 
+console.log(obj.name);
+console.log(obj.age); //prints age
