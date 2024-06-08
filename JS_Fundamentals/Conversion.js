3ef7f72fb79cedb8700d91fce37475e64c30e87e
Mayur <mayursonare18@gmail.com>
2024-06-08 15:45:02 +0530

    js funds II

---
+++ JS_Fundamentals/Conversion.js
@@ -0,0 +1,37 @@
+let a = 10;
+let b = a.toString()
+let c = true
+let d = a - '5'
+let e = a + '5'
+let f = 'a'
+let g = "a"
+
+console.log(a)
+console.log(typeof a)
+
+
+console.log(b)
+console.log(typeof b)
+
+console.log(c)
+console.log(typeof c)
+
+console.log(d)
+console.log(typeof d)
+
+console.log(e)
+console.log(typeof e)
+
+console.log(typeof f)
+console.log(typeof g)
+
+let arr = [[1, "a"], [2, "b"], [3, "c"]]
+let arr2 = [1,2,3,4]
+
+let obj = Object.fromEntries(arr)
+let keys = Object.keys(arr2)
+let values = Object.values(arr2)
+
+console.log(obj)
+console.log(keys)
+console.log(values)
